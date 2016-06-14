44cdbd9c22af4ff7e2998b773d1822f7272ff61c
christian <chris.dillon92@gmail.com>
2016-06-14 13:52:25 -0700

    fixes inconsistency, matches end of section 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,20 @@
-// ale
 var card1 = "queen";
 var card2 = "king";
 var card3 = "king";
 var card4 = "queen";
+
+var select1 = card2;
+var select2 = card1;
+var cardsMatch;
+
+if(select1 === select2){
+    cardsMatch = true;
+}else{
+    cardsMatch = false;
+}
+
+if(cardsMatch){
+    alert("you've found matching cards, yay! :)");
+}else{
+    alert("sorry, those cards don't match :(")
+}
